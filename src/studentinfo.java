e7c653f94d0b7723b7b34d7fe8d1eecbd927d5de
Ishani das <24157020@kiit.ac.in>
2026-01-07 23:31:36 +0530

    COMMIT

---
+++ src/studentinfo.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class studentinfo {
+        public static void main(String[] args){
+            System.out.println("Name: Ishani Das");
+            System.out.println("Rollno: 24157020");
+            System.out.println("Section: CSE 07");
+            System.out.println("Branch: CSE");
+        }
+    }
+
