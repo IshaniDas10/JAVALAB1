f538770a2a000fb095eecc9366ace0837f6b490d
Ishani das <24157020@kiit.ac.in>
2026-01-07 23:24:06 +0530

    COMMIT

---
+++ src/name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class name {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enetr your first name: ");
+        String g=sc.nextLine();
+        System.out.println("Enter your last Name: ");
+        String h=sc.nextLine();
+        System.out.println("Your name is: " + g + h);
+    }
+}
+
+
